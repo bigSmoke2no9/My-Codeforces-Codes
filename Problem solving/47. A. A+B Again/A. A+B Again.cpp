e6cb54b97829a07fe31cfe53289d2fc57625fda0
bigSmoke2no9 <169021442+bigSmoke2no9@users.noreply.github.com>
2024-08-27 20:16:39 +0600

    new code

---
+++ Problem solving/47. A. A+B Again/A. A+B Again.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(--t)
+    {
+        int x;
+        cin >> x;
+
+        int a= x/10;
+        int b= x%10;
+
+        cout << a+b << endl;   
+    }
+    return 0;
+}
