3f7e981d5be70ff2ca2b3210c974a8177ca5c153
Aritra Sarkar <aritroaikya@gmail.com>
2024-11-01 14:26:43 +0600

    Code

--- Problem solving/52. A. Calculating Function/A. Calculating Function.cpp
+++ Problem solving/52. A. Calculating Function/A. Calculating Function.cpp
@@ -2,21 +2,16 @@
 #include<cmath>
 using namespace std;
 
-double f(int n)
-{
-    double sum=0;
-    for(double i=1; i<=n; i++)
-    {
-        sum=sum+(pow(-1,i)*(i));
-    }
-    return sum;
-}
-
 int main()
 {
-    double n;
-    cin >> n;
-    double ans=f(n);
-    cout << ans << endl;
-    return 0;
+    long long int a;
+    cin >> a;
+    if(a%2==0)
+    {
+        cout << a/2 << endl;
+    }
+    else
+    {
+        cout << -(a/2)-1 << endl;
+    }
 }
